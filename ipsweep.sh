7bb8abb86a8b6460369f36bc77e2ed821fa56bb8
Rain <ianryan@pm.me>
2021-10-01 22:49:22 +0100

    Script changed to allow custom IP & error message added for no IP entered.

--- ipsweep.sh
+++ ipsweep.sh
@@ -1,5 +1,12 @@
 #!/bin/bash 
 
+if [ "$1" == "" ]
+then
+echo "You forgot to enter an IP address."
+echo "Syntax: /ipsweep.sh 192.168.0"
+
+else
 for ip in `seq 1 254`; do
-ping -c 1 192.168.0.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
+fi 
